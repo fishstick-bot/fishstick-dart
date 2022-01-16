8fa4903edf3dedd924bb34485bc6b818e16a7a42
vanxhyt <vanxhyt@gmail.com>
2022-01-16 14:06:27 +0000

    remove pdf command

--- lib/commands/fortnite/account/account.dart
+++ lib/commands/fortnite/account/account.dart
@@ -2,7 +2,7 @@ import "package:nyxx_commands/nyxx_commands.dart";
 import "token/token.dart";
 import "exchange_code/exchange_code.dart";
 import "page.dart";
-import "botdata.dart";
+// import "botdata.dart";
 import "receipts.dart";
 import "realname/realname.dart";
 import "username/username.dart";
@@ -15,7 +15,7 @@ final Group accountCommand = Group(
     accessTokenCommand,
     exchangeCodeCommand,
     accountSettingsPageCommand,
-    botDataCommand,
+    // botDataCommand,
     accountReceiptsCommand,
     realNameCommand,
     usernameCommand,
