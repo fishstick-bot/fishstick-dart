84e864f3c4a8badcdc5e91bfb7c7c0782bd0b1a5
Vanxh <71810662+vanxh@users.noreply.github.com>
2022-02-11 16:25:11 +0530

    cooldown check handler

--- lib/utils/commands_handler.dart
+++ lib/utils/commands_handler.dart
@@ -31,24 +31,36 @@ void handleCommandsError(Client client, CommandsPlugin commands) {
     }
 
     if (exception is CheckFailedException) {
-      if (exception.failed.name.contains("premium-check")) {
-        var m = await exception.context.respond(
-          MessageBuilder.content(
-              "You are on cooldown for this command. Please try again in a while."),
-          private: true,
-        );
-        await Future.delayed(
-          Duration(seconds: 3),
-          () async => await m.delete(),
-        );
-        return;
-      }
+      // if (exception.failed.name.contains("cooldown-check")) {
+      //   var m = await exception.context.respond(
+      //     MessageBuilder.content(
+      //         "You are on cooldown for this command. Please try again in a while."),
+      //     private: true,
+      //   );
+      //   await Future.delayed(
+      //     Duration(seconds: 3),
+      //     () async => await m.delete(),
+      //   );
+      //   return;
+      // }
 
       switch (exception.failed.name) {
         case "blacklist-check":
           await exception.context.respond(
             MessageBuilder.content("You are blacklisted from using the bot!"),
             private: true,
+          );
+          break;
+
+        case "cooldown-check":
+          var m = await exception.context.respond(
+            MessageBuilder.content(
+                "You are on cooldown for this command. Please try again in a while."),
+            private: true,
+          );
+          await Future.delayed(
+            Duration(seconds: 3),
+            () async => await m.delete(),
           );
           break;
 
@@ -143,6 +155,8 @@ void handleCommandsCheckHandler(CommandsPlugin commands, int commandsCooldown) {
     CooldownCheck(
       CooldownType.user,
       Duration(seconds: commandsCooldown),
+      1,
+      "cooldown-check",
     ),
   );
 
